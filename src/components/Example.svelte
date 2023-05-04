122aeb219861c7240427c5b64b63a42e86014715
Andrew Aquino <dawneraq@gmail.com>
2023-05-04 11:38:42 -0700

    rm starter code

--- src/components/Example.svelte
+++ src/components/Example.svelte
@@ -1,11 +1,1 @@
-<script>
-</script>
-
-<h1>This is written in a component. You can delete it whenever you want.</h1>
-
-<style>
-  h1 {
-    color: steelblue;
-    margin-bottom: 2rem;
-  }
-</style>
+<p>I'm a component!</p>
