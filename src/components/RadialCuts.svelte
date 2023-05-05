baabedf8e03c4905930ab92dcf5bf9373433fc97
Andrew Aquino <dawneraq@gmail.com>
2023-05-04 18:18:43 -0700

    extend lines past onion exterior

--- src/components/RadialCuts.svelte
+++ src/components/RadialCuts.svelte
@@ -4,16 +4,24 @@
 	export let numCuts;
 	export let radius;
 	export let cutScale;
+
+	let cutTarget = {
+		x: width / 2,
+		y: height,
+	};
 </script>
 
 {#each { length: numCuts } as _, i}
 	{@const theta = cutScale(i)}
 
-	<!-- TODO extend lines past onion exterior -->
+	<!-- where cut intercepts onion's outermost layer -->
+	{@const xIntercept = radius * Math.sin(theta)}
+	{@const yIntercept = radius * Math.cos(theta)}
+
 	<line
-		x1={width / 2 - radius * Math.sin(theta)}
-		y1={height - radius * Math.cos(theta)}
-		x2={width / 2}
-		y2={height}
+		x1={width / 2 - xIntercept * 2}
+		y1={height - yIntercept * 2}
+		x2={cutTarget.x}
+		y2={cutTarget.y}
 	/>
 {/each}
