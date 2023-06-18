bdde9df72ad64a33467fc6f3b3c72a3181313a93
Andrew Aquino <dawneraq@gmail.com>
2023-06-18 13:45:47 -0700

    define quarterOnionArea as integral

--- src/App.svelte
+++ src/App.svelte
@@ -29,10 +29,22 @@
 		return format('.0%')(-n);
 	}
 
-	// TODO for a single vertical cut,
-	//   area = integral of circle from 0 to radius
-	$: area = numCuts;
-	$: console.log({ area });
+	// TODO move this math into external JS file
+	let verticalCutThickness = radius;
+	// TODO for a single vertical cut (i.e., cutting the onion half in half),
+	//   quarterOnionArea = integral of circle height from 0 to r, dx (in first quadrant)
+	//   https://ocw.mit.edu/courses/18-01sc-single-variable-calculus-fall-2010/28c569c5d8e79b7e1a2be1755de42d25_MIT18_01SCF10_Ses70a.pdf
+	//   the expression used for quarterOnionArea here is an antiderivative F(x), and will need to have limits of integration applied as in F(b) - F(a)
+	//     right now F(x) is F(verticalCutThickness) - F(0)
+	$: quarterOnionArea =
+		(radius * radius * Math.asin(verticalCutThickness / radius) +
+			verticalCutThickness *
+				Math.sqrt(
+					radius * radius - verticalCutThickness * verticalCutThickness
+				)) /
+		2;
+	$: console.log({ quarterOnionArea });
+	$: console.log('quarterOnionArea should be', (Math.PI * radius * radius) / 4);
 </script>
 
 <svg {width} {height} viewBox="{-width / 2} 0 {width} {height}">
